4c50ea0bca6804bac632f0e1389d4a3e2753c95a
Marcos Vincius <mvcs1996@hotmail.com>
2025-09-08 20:46:54 -0300

    ajuste final

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link
+          rel="icon"
+          href="favicon.svg"
+          sizes="any"
+          type="image/svg+xml"
+        ></link>
+        <title>Instituto EDMA</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
